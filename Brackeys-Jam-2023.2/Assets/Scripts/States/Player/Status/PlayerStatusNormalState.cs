2a75f82f2514e58e88e7402fcf7989a93a9736c4
laumi73 <laumichael73@gmail.com>
2023-08-27 02:21:55 -0400

    Increased enemy damage

--- Brackeys-Jam-2023.2/Assets/Scripts/States/Player/Status/PlayerStatusNormalState.cs
+++ Brackeys-Jam-2023.2/Assets/Scripts/States/Player/Status/PlayerStatusNormalState.cs
@@ -12,7 +12,7 @@ public class PlayerStatusNormalState : PlayerStatusBaseState
     public override void HandleCollision(Collision2D other) {
         base.HandleCollision(other);
         if (other.gameObject.layer == LayerMask.NameToLayer("Enemy")) {
-            stateMachine.TakeDamage(1f);
+            stateMachine.TakeDamage(5f);
             stateMachine.ChangeState(stateMachine.playerStatusInvulnerableState);
         }
     }
