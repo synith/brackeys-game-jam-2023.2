e535d50d9237c9f5fc76ef70c713c3e02d1d2f3e
Semicolon-GD <maerken@icloud.com>
2023-08-27 03:18:10 +0300

    added new enemies

---
+++ Brackeys-Jam-2023.2/Assets/Scripts/PatrollingEnemy.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PatrollingEnemy : MonoBehaviour
+{
+    Vector2 _startPosition;
+    [SerializeField] Vector2 _direction = Vector2.up;
+    [SerializeField] float _maxDistance = 2;
+    [SerializeField] float _speed = 2;
+
+    SpriteRenderer _spriteRenderer;
+
+    void Start()
+    {
+        _startPosition = transform.position;
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+        _spriteRenderer.flipX = true;
+    }
+
+    void FixedUpdate()
+    {
+        transform.Translate(_direction.normalized * Time.deltaTime * _speed);
+        var distance = Vector2.Distance(_startPosition, transform.position);
+        if (distance > _maxDistance)
+        {
+            transform.position = _startPosition + (_direction.normalized * _maxDistance);
+            _direction *= -1;
+            if (_spriteRenderer.flipX==true)
+            {
+                _spriteRenderer.flipX = false;
+            }
+            else if(_spriteRenderer.flipY==false)
+            {
+             _spriteRenderer.flipX=true;
+            }
+        }
+    }
+
+    public void TakeDamage()
+    {
+        gameObject.SetActive(false);
+    }
+}
